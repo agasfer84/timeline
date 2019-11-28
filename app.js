18470a2f70ed85b6e66bcc5ea2e554b818a0e2dc
avi1984 <a.ignatev@sarrc.ru>
2019-11-28 16:45:33 +0400

    v2019-11-28

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 function main ()
 {
     var self = {};
+    self.data = data;
 
     const startDate = new Date('1984-05-28');
     const stopDate = new Date('1994-09-01');
-    //console.log(data);
+    console.log(self.data[1984][5][28].text); //[5][28].text
 
     Date.prototype.addDays = function(days) {
         var date = new Date(this.valueOf());
@@ -20,7 +21,8 @@ function main ()
         var dateArray = [];
         var currentDate = startDate;
         while (currentDate <= stopDate) {
-            dateArray.push(new Date (currentDate));
+            var date = new Date (currentDate);
+            dateArray.push({"date" : date, "year" : date.getFullYear(), "month" : date.getMonth() + 1, "day" : date.getDate()});
             currentDate = currentDate.addDays(1);
         }
         return dateArray;
@@ -38,16 +40,22 @@ function main ()
 
         days.forEach(function(item, i) {
             var newDayDiv = document.createElement('div');
+            var date = item["date"];
+
             const options = {
                 year: 'numeric',
                 month: 'long',
                 day: 'numeric',
                 weekday: 'long'
             };
-            var dateFormatted = item.toLocaleString("ru", options);
 
-            newDayDiv.innerHTML = "";
-            newDayDiv.setAttribute("class", "day_element");
+            var dateFormatted = date.toLocaleString("ru", options);
+            var day_element_evented_class = (self.data[item.year] && self.data[item.year][item.month] && self.data[item.year][item.month][item.day] && self.data[item.year][item.month][item.day]["text"] !== 'undefined' ) ? " evented" : "";
+
+            newDayDiv.setAttribute("class", "day_element" + day_element_evented_class);
+            newDayDiv.setAttribute("year", item.year);
+            newDayDiv.setAttribute("month", item.month);
+            newDayDiv.setAttribute("day", item.day);
             newDayDiv.setAttribute("title", dateFormatted);
             newDayDiv.setAttribute("onclick", "elementClicked(this);");
             mainBlock.appendChild(newDayDiv);
@@ -60,6 +68,8 @@ function main ()
 function elementClicked(element)
 {
     var day_elements = document.getElementsByClassName("clicked");
+    const eventBlock = document.getElementById("eventBlock");
+    eventBlock.innerHTML = "";
 
     if (day_elements.length > 0) {
         for (var i = 0; i < day_elements.length; i++) {
@@ -73,7 +83,17 @@ function elementClicked(element)
         element.classList.add("clicked");
     }
 
-    console.log(element.title);
+
+    var year = element.getAttribute("year");
+    var month = element.getAttribute("month");
+    var day = element.getAttribute("day");
+
+    var event_text = (data[year] && data[year][month] && data[year][month][day] && data[year][month][day]["text"] !== 'undefined' ) ? data[year][month][day]["text"] : "";
+    console.log(event_text);
+
+    var newEventDiv = document.createElement('div');
+    newEventDiv.innerHTML = '<h4>' + element.title + '</h4>' + '<p>' + event_text + '</p>';
+    eventBlock.appendChild(newEventDiv);
 }
 
 window.onload = function() {
